b672cdf6c893c7db3fe48972781bc1732a19c65b
Akhil184 <akhilteli55@gmail.com>
2024-05-12 18:07:52 +0530

    add slider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,23 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:motion_tab_bar/MotionBadgeWidget.dart';
 import 'package:motion_tab_bar/MotionTabBar.dart';
 import 'package:motion_tab_bar/MotionTabBarController.dart';
+import 'package:heart_overlay/heart_overlay.dart';
+
+final List<String> imgList = [
+ 'assets/images/korean_img.png',
+  'assets/images/korean_img.png',
+  'assets/images/korean_img.png',
+  'assets/images/korean_img.png',
+  'assets/images/korean_img.png'
+
+];
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,29 +27,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late HeartOverlayController heartOverlayController;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   heartOverlayController = HeartOverlayController();
+  // }
+
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
   MotionTabBarController? _motionTabBarController;
-
-  int _selectedIndex = -1;
-
 
   final items = [
     Image.asset('images/drumstick.png'),
     Image.asset('images/pizza.png')
   ];
 
-
-  // CarouselController buttonCarouselController = CarouselController();
   @override
   void initState() {
     super.initState();
-    //// Use normal tab controller
-    // _tabController = TabController(
-    //   initialIndex: 1,
-    //   length: 4,
-    //   vsync: this,
-    // );
-
-    //// use "MotionTabBarController" to replace with "TabController", if you need to programmatically change the tab
+    heartOverlayController = HeartOverlayController();
     _motionTabBarController = MotionTabBarController(
       initialIndex: 0,
       length: 4, vsync: this,
@@ -168,72 +180,194 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           tabIconSelectedSize: 26.0,
           tabSelectedColor:Colors.pink,
           tabIconSelectedColor: Colors.white,
-          tabBarColor: Colors.black12,
+          tabBarColor: Colors.deepPurpleAccent,
           onTabItemSelected: (int value) {
             setState(() {
               _motionTabBarController!.index = value;
             });
           },
         ),
-        body: Container(
-    height:responsive.height*0.76,
+        body:Stack(children: [
+
+  Container(
+    height:600,
+    child: CarouselSlider(
+    items: imageSliders,
+    carouselController: _controller,
+    options: CarouselOptions(
+    autoPlay:false,
+    height:800,
+
+    enlargeCenterPage: true,
+    onPageChanged: (index, reason) {
+    setState(() {
+    _current = index;
+    });
+    }),
+    ),
+    ),
+          Padding(padding:EdgeInsets.only(top:30),
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.asMap().entries.map((entry) {
+                return GestureDetector(
+                  onTap: () => _controller.animateToPage(entry.key),
+                  child: Container(
+                    width: 50.0,
+                    height: 5.0,
+                    margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 4.0),
+                    decoration: BoxDecoration(
+                      // border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(15),
+                        color: (Theme.of(context).brightness == Brightness.dark
+                            ? Colors.grey
+                            : Colors.pink)
+                            .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+      Padding(padding:EdgeInsets.only(top:460,left:50),
+      child:Container(
+        width:100,
+          margin: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(3.0),
+          decoration: BoxDecoration(
+            color:Colors.black,
+            border: Border.all(color: Colors.grey,),
+            borderRadius: BorderRadius.all(
+                Radius.circular(
+                    30.0) //                 <--- border radius here
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(
+                Icons.star_outlined,
+                color: Colors.grey,
+                size: 20.0.sp,
+              ),
+              Text('29,930', style: TextStyle(
+                color: Colors.white,
+                fontSize: 12.sp,
+              ),),
+            ],
+          ),
+      ),
+      ),
+      Padding(padding:EdgeInsets.only(top:530,left:70),
+          child:Text(
+            '목이길어슬픈기린님의 새로운 스팟',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12.sp,
+            ),
+          ),
+      ),
+          Padding(padding:EdgeInsets.only(top:550,left:70),
+            child:Text(
+              '2km 거리에 있음',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 12.sp,
+              ),
+            ),
+          ),
+      // HeartOverlay(
+      //   controller: heartOverlayController,
+      //   duration: const Duration(seconds: 5),
+      //   backgroundColor: Colors.black,
+      //   tapDownType: TapDownType.single,
+      //   // verticalOffset: 20,
+      //   // horizontalOffset: -100,
+      //   enableGestures: true,
+      //   cacheExtent: 30,
+      //   splashAnimationDetails: const SplashAnimationDetails(
+      //     enableSplash: true,
+      //     animationDuration: Duration(seconds: 3),
+      //   ),
+      //   icon: const Icon(
+      //     Icons.favorite,
+      //     color: Colors.redAccent,
+      //     size: 100,
+      //   ),
+      //   onPressed: (numberOfHearts) {
+      //     // Do something with the number of hearts or do something whenever the icon appears
+      //   },
+      // ),
+    Padding(
+    padding: const EdgeInsets.all(50.0),
+    child: ElevatedButton(
+    onPressed: () {
+    // Get the center offset
+    // Offset centerOffset =
+    //     heartOverlayController.getCenterScreenOffset(context);
+
+    double iconSize = heartOverlayController.getSize() * 2;
+    double height = heartOverlayController.getHeight() ??
+    MediaQuery.of(context).size.height;
+    double width = heartOverlayController.getWidth() ??
+    MediaQuery.of(context).size.width;
+
+    Offset offset = Offset(
+    // DX
+    (Random().nextDouble() * width).clamp(
+    iconSize,
+    width - iconSize,
+    ),
+    // DY
+    (Random().nextDouble() * height).clamp(
+    iconSize,
+    height - iconSize - 100,
+    ), // - 100 is for the button padding
+    );
+
+    // offset += centerOffset; // You can add offsets
+
+    // Show the icon at the provided offset
+    heartOverlayController.showIcon(
+    offset: offset,
+    );
+
+    // Changes to the emoji on the second click
+    heartOverlayController.changeIcon(
+    icon: const Text('👀'), // Required
+    size: 200, // Not required
+    horizontalOffset: 10, // Not required
+    verticalOffset: 30, // Not required
+    );
+    },
+    child: const Text('Show Icon'),
+    ),
+    )
+    ]),
+
+    );
+  }
+}
+
+
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Flexible(
+  child: Container(
+      height:3000,
+    width:1000,
     decoration: BoxDecoration(
     border: Border.all(color: Colors.grey),
     borderRadius: BorderRadius.circular(15),
     image: DecorationImage(
-    image:AssetImage("assets/images/img.png"),
-    fit:BoxFit.fill,
+    image:AssetImage(item),
+    fit:BoxFit.fill
+    // fit:BoxFit.cover,
     ),
     ),
-    child:Card(
-    color: Colors.transparent,
-    elevation: 4.0,
-    margin: EdgeInsets.all(10),
-    child:ListView.builder(
-      scrollDirection:Axis.horizontal,
-    itemCount:5,
-    itemBuilder: (context, index) {
-    return Column(
-      crossAxisAlignment:CrossAxisAlignment.start,
-      children: [
-        Container(
-    height:10,
-    child: InkWell(
-      onTap: () {
-        setState(() {
-          _selectedIndex = index;
-
-
-        });
-      },
-      highlightColor: Colors.blue.withOpacity(0.4),
-      child: Container(
-        width:responsive.width*0.18,
-        height:2,
-        padding: EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          color:_selectedIndex == index ? Colors.pink : Colors.black,
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-      ),
-    ),
-    ),
-
-
-      ],
-    );
-
-    }
-
-          ),
-        ),
-        ),
-
-          );
-
-
-  }
-}
+    margin: EdgeInsets.all(5.0),
+  ),
+))
+    .toList();
 
 
     class MainPageContentComponent extends StatelessWidget {
